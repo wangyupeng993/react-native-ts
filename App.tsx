10b0f6035ec27c4dad394c74f980e1d8501c49f4
wangyupeng <Ljj13559497175>
2020-08-23 00:03:37 +0800

    获取状态来高度

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
-import React,{Fragment} from 'react';
-// import {View} from "react-native";
+import React,{Fragment,useState,useEffect} from 'react';
+import {View,Text,StatusBar,Dimensions,NativeModules,Platform} from "react-native";
+const {StatusBarManager} = NativeModules;
 
 const App = () => {
-    return (<Fragment></Fragment>);
+    const {width,height} = Dimensions.get('window');
+    const [BarHeight,setBarHeight] = useState(0);
+
+    useEffect(() => {
+        if (Platform.OS === 'ios') {
+            console.log(StatusBarManager['HEIGHT'],'StatusBarManager============');
+            setBarHeight(StatusBarManager['HEIGHT']);
+        };
+        return () => {};
+    },[]);
+    return (<Fragment>
+        <View style={{width: width,height: height,backgroundColor: '#8dc63f',paddingTop: BarHeight}}>
+            <StatusBar backgroundColor={'blue'}></StatusBar>
+            <Text>width: {width}</Text>
+            <Text>height: {height}</Text>
+        </View>
+    </Fragment>);
 };
 
 export default App;
