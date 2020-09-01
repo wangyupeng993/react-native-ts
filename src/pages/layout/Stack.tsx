cbf645b6e331068231522bb081f221e7c7b9fc14
wangyupeng <Ljj13559497175>
2020-09-02 01:33:28 +0800

    路由嵌套----2

---
+++ src/pages/layout/Stack.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {createStackNavigator} from "@react-navigation/stack";
+import {StackRoutes} from "../../router/routes";
+
+const Stack = createStackNavigator();
+const StackNavigate = () => {
+    return (<Stack.Navigator initialRouteName={'TabNavigate'}>
+        {StackRoutes.map(item => {
+            return  <Stack.Screen key={item.path} name={item.name}
+                                  options={item.options}
+                                  component={item.component} />
+        })}
+    </Stack.Navigator>);
+};
+
+export default StackNavigate;
--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -3,23 +3,15 @@ import React,{Suspense} from "react";
 import {Text} from "react-native";
 import {Provider} from "react-redux";
 import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-import {StackRoutes} from "../../router/routes";
+import StackNavigate from "./Stack";
 import store from "../../redux";
 
-const Stack = createStackNavigator();
 const AppMain = () => {
     return (
         <Provider store={store}>
             <NavigationContainer>
                 <Suspense fallback={<Text>....</Text>}>
-                    <Stack.Navigator initialRouteName={'TabNavigate'}>
-                        {StackRoutes.map(item => {
-                           return  <Stack.Screen key={item.path} name={item.name}
-                                                 options={item.options}
-                                                 component={item.component} />
-                        })}
-                    </Stack.Navigator>
+                    <StackNavigate />
                 </Suspense>
             </NavigationContainer>
         </Provider>
